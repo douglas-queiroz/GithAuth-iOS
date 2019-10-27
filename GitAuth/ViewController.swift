905355da8322739e5ef691b1ef6bc07c19192af6
Douglas Queiroz <douglasq16@gmail.com>
2019-10-27 23:19:28 +0000

    Initial Commit

---
+++ GitAuth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitAuth
+//
+//  Created by Douglas Queiroz on 10/27/19.
+//  Copyright © 2019 Douglas Queiroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
